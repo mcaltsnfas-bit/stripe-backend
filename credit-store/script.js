37f25e2d9ca005992c3c1f85f2d92e3cb81b815a
Jainil <mcalts.nfas@gmail.com>
2026-05-02 11:51:06 +0100

    fetch updated

--- credit-store/script.js
+++ credit-store/script.js
@@ -1,11 +1,13 @@
 async function buyCredits(amount) {
   try {
-    const res = await fetch("https://mcalts.co.uk/create-checkout", {
+    const res = await fetch("http://mcalts.co.uk/create-checkout", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ credits: amount })
+      body: JSON.stringify({
+        credits: amount
+      })
     });
 
     const data = await res.json();
@@ -13,8 +15,8 @@ async function buyCredits(amount) {
     if (data.url) {
       window.location.href = data.url;
     } else {
-      console.error("Checkout error response:", data);
-      alert(data.error || "Error creating checkout");
+      console.log("Server response:", data);
+      alert("Error creating checkout");
     }
 
   } catch (err) {
